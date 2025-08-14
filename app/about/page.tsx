b185697581af5bba922c37dceac82d0262eb17f3
Prem Chand Saini <premchandsaini779@gmail.com>
2025-08-14 15:22:41 +0530

    fix about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,11 @@
-import HeaderWithSlider from "@/components/header-with-slider";
 import AboutSection from "@/components/about-section";
-import Footer from "@/components/footer";
 import HowItWorksSection from "@/components/how-it-works-section";
 
 export default function AboutPage() {
   return (
     <div className="min-h-screen bg-background">
-      <HeaderWithSlider />
       <AboutSection />
       <HowItWorksSection />
-      <Footer />
     </div>
   );
 }
